862b8ffc9f6de65cdd2ef478e0c1dd80c3a90823
Kevin Yokley <kyokley2@gmail.com>
2019-08-04 20:37:42 -0500

    Ignore reading from stdin message

--- pgcli/pgcli.sh
+++ pgcli/pgcli.sh
@@ -3,4 +3,4 @@
 what=-
 test "$@" && what="$@"
 PLUGIN_DIR='/pager/plugin'
-exec vim -u NONE -S $PLUGIN_DIR/pgcli.vim -c "let &runtimepath='$PLUGIN_DIR,' . &runtimepath" -c Less $what
+exec vim --not-a-term -u NONE -S $PLUGIN_DIR/pgcli.vim -c "let &runtimepath='$PLUGIN_DIR,' . &runtimepath" -c Less $what
